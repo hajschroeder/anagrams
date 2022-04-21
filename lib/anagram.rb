fd555b817ee7946e63774fade03a8a4519ca51bd
Hans Schroeder <hajschroeder@gmail.com>
2022-04-21 13:43:02 -0700

    refactor code to achieve passing tests. All 3 tests now green

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Anagram 
   def word_checker(word_one, word_two)
-    word_one.chars.sort.to_s.match?(word_two.chars.sort.to_s)
+    word_one.downcase.chars.sort.to_s == word_two.downcase.chars.sort.to_s
 
   end
 end
