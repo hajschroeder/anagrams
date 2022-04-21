e0e9a3312cae1906cd70d861f4da4073861d38d3
Hans Schroeder <hajschroeder@gmail.com>
2022-04-21 13:06:26 -0700

    failure with first test found, not returning false when expected

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -7,6 +7,9 @@ describe Anagram do
   it 'returns true if words match letters' do
     expect(words.word_checker("lamb", "balm")).to eq(true)
   end
+  it 'returns false for non-anagrams' do
+    expect(words.word_checker("foo", "bar")).to eq(false)
+  end
   it 'accounts for capitalization' do
     expect(words.word_checker("LaMb", "BAlm")).to eq(true)
   end
