48352aa7358cc6171bfbec1e7696cf7da8b4507f
Hans Schroeder <hajschroeder@gmail.com>
2022-04-22 12:05:55 -0700

    refactor tests to condense examples

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,19 +4,19 @@ require 'rspec'
 describe Anagram do
   describe '#word_checker' do 
   words = Anagram.new()
-  it 'returns true if words match letters' do
+  it 'returns true if words match letters and false when they do not' do
     expect(words.word_checker("lamb", "balm")).to eq(true)
-  end
-  it 'returns false for non-anagrams' do
     expect(words.word_checker("foo", "bar")).to eq(false)
   end
   it 'accounts for capitalization' do
     expect(words.word_checker("LaMb", "BAlm")).to eq(true)
   end
-  it 'returns false when input has no vowels' do 
-    word.compare << "wrld"
-    compare = word.compare
-    expect(compare). to eq(false)
+  end
+  describe '#letter_checker' do 
+  compare =  Anagram.new()
+  it 'returns true if the word contains no vowels and false for ones that do' do
+    expect(compare.letter_checker("hllwrld")).to eq(true)
+    expect(compare.letter_checker("foobarbaz")).to eq(false)
   end
 end
 end
