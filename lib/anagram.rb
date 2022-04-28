7d611a48d474e3ea6c0f3ba1d6a4f15f774d92c1
Hans Schroeder <hajschroeder@gmail.com>
2022-04-28 13:27:55 -0700

    refactor code to accept two words as args. Test passes

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,16 +4,15 @@ class Anagram
     word_one_str = word_one.downcase.chars.sort.join.to_s
     word_two_str = word_two.downcase.chars.sort.join.to_s
     str_comp = word_one_str == word_two_str
-    #if str_comp == false
 
 
   end
   
-  def letter_checker(input)
-    input = input.chars.keep_if { |v| v =~ /[aeiou]/ }
-    input == []
+  def letter_checker(input_one, input_two)
+    first_input = input_one.chars.keep_if { |v| v =~ /[aeiou]/ }
+    second_input = input_two.chars.keep_if{ |v| v =~ /|aeiou|/}
+    input_comp = first_input == [] || second_input == []
   end
-
 
 end
     
