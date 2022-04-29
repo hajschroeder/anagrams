42b999166327c55eed54f11b99aa6ba98ed55336
Hans Schroeder <hajschroeder@gmail.com>
2022-04-29 11:57:00 -0700

    update code per last test. Green

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,14 @@
 require 'pry'
 class Anagram 
   def word_checker(word_one, word_two)
-    word_one_str = word_one.downcase.chars.sort.join.to_s
-    word_two_str = word_two.downcase.chars.sort.join.to_s
+    word_one_str = word_one.downcase.chars.sort.join.to_s.delete(' ')
+    word_two_str = word_two.downcase.chars.sort.join.to_s.delete(' ')
     str_comp = word_one_str == word_two_str
-
-
   end
   
-  def letter_checker(input_one, input_two)
-    first_input = input_one.chars.keep_if { |v| v =~ /[aeiou]/ }
-    second_input = input_two.chars.keep_if{ |v| v =~ /|aeiou|/}
+  def letter_checker(word_one, word_two)
+    first_input = word_one.chars.keep_if { |v| v =~ /[aeiou]/ }
+    second_input = word_two.chars.keep_if { |v| v =~ /[aeiou]/}
     input_comp = first_input == [] || second_input == []
   end
 
