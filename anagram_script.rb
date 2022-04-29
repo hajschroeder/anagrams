c0f1e064f6806995b7ba4634037580834b8e581f
Hans Schroeder <hajschroeder@gmail.com>
2022-04-29 12:22:22 -0700

    updated name of script file to be more descriptive

--- anagram_script.rb
+++ anagram_script.rb
@@ -4,18 +4,23 @@ require './lib/anagram'
 words = Anagram.new()
 puts "Please Enter a word"
 word_one = gets.chomp
-is_word = words.letter_checker(word_one)
-if is_word
-  puts "that's not a valid input"
-
 puts "And another please"
 word_two = gets.chomp
 
+puts "#{word_one}, #{word_two}"
+is_word = words.letter_checker(word_one, word_two)
 is_anagram = words.word_checker(word_one, word_two)
+is_antigram = words.antigram_checker(word_one, word_two)
+if is_word
+  puts "one of these is not a word, we need vowels"
 end
 if is_anagram
   puts "these are anagrams"
-elsif is_anagram == false
+else
   puts "nope, not an anagram"
 end
+if is_antigram
+  puts "these are actually antigrams"
+end
+
 
