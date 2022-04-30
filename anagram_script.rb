c5ac85a2ac271ebaf88634aae7fe6f846f3969ca
Hans Schroeder <hajschroeder@gmail.com>
2022-04-30 13:18:35 -0700

    initial commit of a branch with code refactored to include instance variables

--- anagram_script.rb
+++ anagram_script.rb
@@ -1,25 +1,24 @@
 #!/usr/bin/env ruby
 require './lib/anagram'
 
-words = Anagram.new()
-puts "Please Enter a word"
+puts "please enter a word"
 word_one = gets.chomp
-puts "And another please"
+puts "and another please"
 word_two = gets.chomp
-
+words = Anagram.new(word_one, word_two)
 puts "#{word_one}, #{word_two}"
-is_word = words.letter_checker(word_one, word_two)
-is_anagram = words.word_checker(word_one, word_two)
-is_antigram = words.antigram_checker(word_one, word_two)
+is_word = words.vowel_checker?
+is_anagram = words.word_checker?
+is_antigram = words.antigram_checker?
 if is_word
-  puts "one of these is not a word, we need vowels"
+  puts "one of these is not a word"
 end
 if is_anagram
-  puts "these are anagrams"
-else
+  puts "you've got yerself an anagram"
+else 
   puts "nope, not an anagram"
 end
-if is_antigram
+if is_antigram 
   puts "these are actually antigrams"
 end
 
